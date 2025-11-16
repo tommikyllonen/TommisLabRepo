c464b675cfe6c007fb7aa81dadf367d26d15769d
Tommi Kyllönen <tommi.kyllonen@gmail.com>
2025-11-16 15:35:46 +0200

    Tasks up 1-3 done

--- A7/A7_T1.py
+++ A7/A7_T1.py
@@ -1,32 +1,25 @@
 def main():
     print("Program starting.")
     print("Collect positive integers.")
-
-    integers = [] 
-    user_input = 0
-    while (user_input >= 0):
-        try:
-            user_input = int(input("Insert positive integer (negative stops): "))
-            if user_input <= 0:
+    positiveIntegers = []
+    userInput = 1
+    while(userInput > 0):
+            userInput = int(input("Insert positive integer(negative stops): "))
+            if (userInput > 0):
+                positiveIntegers.append(userInput)
+            else: 
                 break
-            else:
-                integers.append(user_input)
-        except ValueError:
-            print("Invalid input. Please enter an integer.")
-
     print("Stopped collecting positive integers.")
-
-    if integers:
-        print(f"Displaying {len(integers)} integers:")
-        for index, value in enumerate(integers):
-            ordinal = index + 1
-            print(f"- Index {index} => Ordinal {ordinal} => Integer {value}")
+    if (len(positiveIntegers) == 0):
+         print("No integers to display.")
     else:
-        print("No integers to display.")
+        print(f"Displaying {len(positiveIntegers)} integers:")
+        for i in range(len(positiveIntegers)):
+             print(f"- Index {i} => Ordinal {i + 1} => Integer {positiveIntegers[i]}")
+    print("program ending")
 
-    print("Program ending.")
 
 
-# Run the program
 if __name__ == "__main__":
     main()
+    

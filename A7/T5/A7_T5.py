1383c91e7a2e8d3dc6db9e0354147342cd383ea5
Tommi Kyllönen <tommi.kyllonen@gmail.com>
2025-11-17 21:30:47 +0200

    Added 3,4,5

--- A7/T5/A7_T5.py
+++ A7/T5/A7_T5.py
@@ -1,9 +1,10 @@
 
 WEEKDAYS = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturnday", "Sunday",)
 
-def readTimestamps(PFilename: str, timestamps) -> None:
+def readTimestamps(PFilename: str, timestamps, dailyUsage) -> None:
     tempRows: list[str] = []
     print('Reading file "{}".'.format(PFilename))
+    dailyUsage.clear()
     timestamps.clear()
     file = open(PFilename, "r")
     while True:
@@ -19,19 +20,26 @@ def readTimestamps(PFilename: str, timestamps) -> None:
     file.close()
     #analyse timestamps
     tempList = []
+    tempDailyList = []
     for weekday in WEEKDAYS:
+        weekday = weekday
+        consumption = 0
+        price = 0
         for row in tempRows:
             if row.startswith(weekday):
                 listItem = row.split(";")
+                # add items to total
+                consumption += float(listItem[2])
+                price += float(listItem[2]) * float(listItem[3])
                 tempList.append(listItem)
+        tempDailyList.append([weekday, consumption, price])
+    dailyUsage.extend(tempDailyList)
     timestamps.extend(tempList)
-    # return tempList
     return None
 
 def TIMESTAMP() -> None:
     print("Timestamp module.")
     return None
-
 
 def displayTimestamps(results: list[list[str]]) -> None:
     print("Electricity usage:")
@@ -44,19 +52,31 @@ def displayTimestamps(results: list[list[str]]) -> None:
         print(f" - {weekday} {hour}:00, price {price}, consumption {consumption} kWh, total {round(total, 2)}  €")
     return None
 
+
+def displayAverages(dailyUsage) -> None:
+    print("Analysing timestamps.")  
+    print("Displaying results.")
+    print("### Electricity consumption summary ###")
+    for day in dailyUsage:
+        weekday = day[0]
+        usage = f"{day[1]:.2f}"
+        cost = f"{day[2]:.2f}"
+        print(f" - {weekday} usage {usage} kWh, cost {cost} €")
+    print("### Electricity consumption summary ###")
+
 def main() -> None:
     print("Program starting.")
+    dailyUsage = []
     timestamps: list[str] = []
     PResults: list[str] = []
     fileName = input("Insert filename: ")
-    readTimestamps(fileName, timestamps)
-    # results = analyseTimestamps(timestamps)
-    displayTimestamps(timestamps)
-    # print(results)
-    timestamps = []
-    PResults = []
+    readTimestamps(fileName, timestamps, dailyUsage)
+    # displayTimestamps(timestamps)
+    displayAverages(dailyUsage)
+    print("Program ending.")
     return None
 
 
 if __name__ == "__main__":
     main()
+
